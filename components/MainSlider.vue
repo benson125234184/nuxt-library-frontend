038e5a38c9404b7d8b8ce1b8fd527aebc432a7b0
Jaymes <air2997329@gmail.com>
2025-06-03 22:09:43 +0800

    更新 sidebar 結構、新增借閱證/書評等頁面，刪除舊 BookSearch

--- components/MainSlider.vue
+++ components/MainSlider.vue
@@ -1,32 +1,25 @@
 <template>
-  <!-- <div class="slider-container">
-    <button class="arrow left" @click="prev">‹</button> -->
+  <div class="slider-container">
 
     <div class="slider">
       <img :src="images[current]" alt="slide image" />
     </div>
-
-    <!-- <button class="arrow right" @click="next">›</button>
-  </div> -->
+  </div>
 
   <div class="dots">
-  <span
-    v-for="(img, index) in images"
-    :key="index"
-    :class="{ active: index === current }"
-    @click="current = index"
-  />
-</div>
+    <span v-for="(img, index) in images" :key="index" :class="{ active: index === current }" @click="current = index" />
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 
 const images = [
-  'https://static.vecteezy.com/system/resources/thumbnails/000/525/908/small_2x/gde7_tf5t_180914.jpg',
-  'https://static.vecteezy.com/system/resources/thumbnails/000/087/792/small_2x/playground-slide-vector.jpg',
-  'https://cdn11.bigcommerce.com/s-tddaa/images/stencil/1280x1280/products/1898/8598/7_Slide_Retouched_902-315__72528.1469218489.jpg?c=2',
+  'https://images.unsplash.com/photo-1534774592507-488885376ad3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1600431521340-491eca880813?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1522211988038-6fcbb8c12c7e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 ]
+
 const current = ref(0)
 
 // const next = () => {
@@ -50,23 +43,26 @@ onMounted(() => {
   align-items: center;
   justify-content: center;
   position: relative;
-  max-width: 1500px;
+  height: 300px;
+  /* ✅ 明確高度 */
   margin: 0 auto;
   padding: 20px 0;
 }
 
 .slider {
-  width: 100%;
-  /* height: 400px; */
+  flex: 1;
+  /* ✅ 自動撐滿父層寬度 */
+  height: 100%;
+  /* ✅ 撐滿容器高度 */
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
 .slider img {
-  max-width: 100%;
-  max-height: 300px;
-  object-fit: fill;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
 }
@@ -112,5 +108,4 @@ onMounted(() => {
 .dots span.active {
   background-color: #333;
 }
-
 </style>
